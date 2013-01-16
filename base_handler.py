ab4c9ddcfa8107991d65dcaace74fcce8e49b519
Daniel Hermes <dhermes@google.com>
2013-01-15 18:19:13 -0800

    Updating indents and adding missing returns to jinja2 property.

--- base_handler.py
+++ base_handler.py
@@ -26,16 +26,20 @@ class BaseHandler(webapp2.RequestHandler):
 
     @webapp2.cached_property
     def jinja2(self):
-        """Cached property holding a Jinja2 instance."""
+        """Cached property holding a Jinja2 instance.
+
+        Returns:
+            A Jinja2 object for the current app.
+        """
         return jinja2.get_jinja2(app=self.app)
 
     def render_response(self, template, **context):
         """Use Jinja2 instance to render template and write to output.
 
         Args:
-          template: filename (relative to $PROJECT/templates) that we are
-              rendering.
-          context: keyword arguments corresponding to variables in template.
+            template: filename (relative to $PROJECT/templates) that we are
+                rendering.
+            context: keyword arguments corresponding to variables in template.
         """
         rendered_value = self.jinja2.render_template(template, **context)
         self.response.write(rendered_value)
